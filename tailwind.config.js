26703a2152c5363b7ba747a8614abb9f00be8455
imadsinjer52 <imadsinjer52@gmail.com>
2025-10-11 08:09:18 +0200

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // Enable class-based dark mode
   theme: {
-    extend: {
-      colors: {
-        // You can add custom colors here if needed
-      },
-      transitionProperty: {
-        'colors': 'background-color, border-color, color, fill, stroke',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
